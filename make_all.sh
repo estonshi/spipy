cf4b73950012f4c47ef4002b28228eefa18c6d53
YC.S <shiyc1994@gmail.com>
2018-02-10 18:45:32 +0800

    v1.0
    All code tested on LINUX and MAC!

--- make_all.sh
+++ make_all.sh
@@ -30,25 +30,36 @@ fi
 echo "==> compile image/bhtsne_source"
 cd $root_folder/image/bhtsne_source
 g++ sptree.cpp tsne.cpp tsne_main.cpp -o bh_tsne -O2
+chmod u+x bh_tsne
 
 echo "==> compile merge/template_emc/src"
 cd $root_folder/merge/template_emc/src
+chmod u+x compile.sh ../new_project
 if [ $sys = "Linux" ]
 then
 	mpicc -fopenmp recon.c setup.c max.c quat.c interp.c -o emc_LINUX -I ./ -lgsl -lgslcblas -lm -O3
+	chmod u+x emc_MAC
 elif [ $sys = "Darwin" ]
 then
 	$mygcc -fopenmp recon.c setup.c max.c quat.c interp.c -o emc_MAC -I ./ -lgsl -lgslcblas -lm -O3 -lmpi
+	chmod u+x emc_MAC
 fi
 
 echo "==> compile simulate/src"
 cd $root_folder/simulate/src
+chmod u+x compile.sh ../code/make_densities.py ../code/make_detector.py ../code/make_intensities.py
 if [ $sys = "Linux" ]
 then
 	mpicc -fopenmp make_data.c -o make_data_LINUX -I ./ -lgsl -lgslcblas -lm -O3
+	chmod u+x make_data_LINUX
 elif [ $sys = "Darwin" ]
 then
 	$mygcc -fopenmp make_data.c -o make_data_MAC -I ./ -lgsl -lgslcblas -lm -O3
+	chmod u+x make_data_MAC
 fi
+
+echo "==> others"
+cd $root_folder/phase
+chmod u+x ./template_2d/new_project ./template_3d/new_project
 
 echo "Complete!"
